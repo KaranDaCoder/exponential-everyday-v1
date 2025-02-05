819957fbd7d3f29ddc573a0ae31c3bf00e1f3c6b
Karan Khot <karankhot@Karans-MacBook-Air.local>
2025-02-04 20:45:33 -0600

    check-app-error

--- src/app/(mainLayout)/layout.jsx
+++ src/app/(mainLayout)/layout.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import Sidebar from './_components/Sidebar'
-import BreadCrumbs from './_components/Breadcrumbs'
 import MobileNavLinks from './_components/MobileNavLinks'
-import { registerUser } from '@/lib/registerUser'
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
 
 
 const MainAppLayout = async ({ children }) => {
- const session = await auth()
- if(!session) {
-  redirect('/')
- }
+
  return (
   <main className="flex">
    {/* Sidebar - only visible on larger screens */}
